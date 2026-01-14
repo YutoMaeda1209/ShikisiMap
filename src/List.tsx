145eb1a7cf4177a9bf6aaca88e37e7b85fa2909e
Yuto Maeda <uvtsas@gmail.com>
2026-01-14 09:10:47 +0000

    Create List component

---
+++ src/List.tsx
@@ -0,0 +1,16 @@
+import "./List.css";
+
+function List() {
+    return (
+        <div id="pointList">
+            <h2>ShikisiMap</h2>
+            <ul>
+                <li>Point 1</li>
+                <li>Point 2</li>
+                <li>Point 3</li>
+            </ul>
+        </div>
+    );
+}
+
+export default List;
