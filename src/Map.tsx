280dc7dda31b02a069a70805eaeca9d89bf1f932
Yuto Maeda <uvtsas@gmail.com>
2026-01-14 09:10:29 +0000

    Create Map component

---
+++ src/Map.tsx
@@ -0,0 +1,33 @@
+import type { Feature, GeoJsonObject, Geometry } from 'geojson';
+import type { Layer } from 'leaflet';
+import "leaflet/dist/leaflet.css";
+import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
+import geoJsonRowData from "./data.json";
+import "./Map.css";
+
+interface ShikisiProperties {
+    popupContent?: string;
+    [key: string]: unknown;
+}
+
+const geoData = geoJsonRowData as GeoJsonObject;
+
+function Map() {
+    const onEachFeature = (feature: Feature<Geometry, ShikisiProperties>, layer: Layer) => {
+        if (feature.properties && typeof feature.properties.popupContent === 'string') {
+            layer.bindPopup(feature.properties.popupContent);
+        }
+    };
+
+    return (
+        <MapContainer center={[35.676423, 139.650027]} zoom={7} zoomControl={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <GeoJSON data={geoData} onEachFeature={onEachFeature} />
+        </MapContainer>
+    );
+}
+
+export default Map;
