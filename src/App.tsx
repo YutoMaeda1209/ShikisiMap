3d06d9606f755080757683a8f80751b6e2d7eab0
Yuto Maeda <uvtsas@gmail.com>
2026-01-14 09:11:36 +0000

    Refactor App component structure and update CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import './App.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import "leaflet/dist/leaflet.css"
+import './App.css';
+import List from './List';
+import Map from './Map';
 
 function App() {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
-        </MapContainer>
-    )
+        <div id="app">
+            <List />
+            <Map />
+        </div>
+    );
 }
 
-export default App
+export default App;
