9f57bcc39c97b5b4f2733a64e072078557b51b8d
Yuto Maeda <uvtsas@gmail.com>
2026-01-15 07:09:44 +0000

    Add function to change map center

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,27 +1,50 @@
 import type { Feature, GeoJsonObject, Geometry } from 'geojson';
-import type { Layer } from 'leaflet';
+import type { LatLng, Layer } from 'leaflet';
 import "leaflet/dist/leaflet.css";
-import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { GeoJSON, MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import geoJsonRowData from "./data.json";
 import "./Map.css";
 import type { GeoProperties } from './types';
 
 const geoData = geoJsonRowData as GeoJsonObject;
 
-function Map() {
-    const onEachFeature = (feature: Feature<Geometry, GeoProperties>, layer: Layer) => {
-        layer.bindPopup(feature.properties.name);
-    };
+function LocationMarker() {
+  const [position, setPosition] = useState<LatLng | null>(null);
+  const map = useMapEvents({
+    locationfound(event) {
+      setPosition(event.latlng)
+      map.flyTo(event.latlng, map.getZoom())
+    },
+    locationerror(err) {
+      alert(err.message);
+    },
+  });
 
-    return (
-        <MapContainer center={[35.676423, 139.650027]} zoom={7} zoomControl={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <GeoJSON data={geoData} onEachFeature={onEachFeature} />
-        </MapContainer>
-    );
+  useEffect(() => {
+    map.locate()
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position} />
+  );
+}
+
+function Map() {
+  const onEachFeature = (feature: Feature<Geometry, GeoProperties>, layer: Layer) => {
+    layer.bindPopup(feature.properties.name);
+  };
+
+  return (
+    <MapContainer center={[35.676423, 139.650027]} zoom={14} zoomControl={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <GeoJSON data={geoData} onEachFeature={onEachFeature} />
+      <LocationMarker />
+    </MapContainer>
+  );
 }
 
 export default Map;
